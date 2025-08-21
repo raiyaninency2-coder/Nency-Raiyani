b14d62309494b90daff34f0a49d8448316706daa
raiyaninency2-coder <raiyaninency2@gmail.com>
2025-08-21 19:35:59 +0530

    Add files via upload

---
+++ nency18.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+     float area,perimeter,length;
+     printf("\n Enter length");
+     scanf("%f",&length);
+     perimeter=4*length;
+     printf("\n perimeter=%f",perimeter);
+     area=length*length;
+     printf("\n area=%f",area);
+     getch();
+
+}
