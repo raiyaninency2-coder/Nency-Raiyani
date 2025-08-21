896b0e264d99c8e451b28ac4b8529915d5d40401
raiyaninency2-coder <raiyaninency2@gmail.com>
2025-08-21 19:24:48 +0530

    Add files via upload

---
+++ nency6.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,c,d,e,f;
+    printf("Enter two numbers:");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("c=%d",c);
+    d=a-b;
+    printf("d=%d",d);
+    e=a*b;
+    printf("e=%d",e);
+    f=a/b;
+    printf("f=%d",f);
+    getch();
+}
