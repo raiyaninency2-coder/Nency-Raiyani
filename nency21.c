cf76b3e416fb671d5bee0c56a4e82d351422ae83
raiyaninency2-coder <raiyaninency2@gmail.com>
2025-08-21 19:38:50 +0530

    Add files via upload

---
+++ nency21.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float h,l,area;
+    printf("Enter l and h :");
+    scanf("%f%f",&l,&h);
+    area=0.5*h*l;
+    printf("\n area=%f",area);
+    getch();
+}
