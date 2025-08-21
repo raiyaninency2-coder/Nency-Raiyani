043b4efb0ae520f343d5258d3d43d12c0e035de3
raiyaninency2-coder <raiyaninency2@gmail.com>
2025-08-21 19:28:39 +0530

    Add files via upload

---
+++ nency10.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int dollors,rupees;
+    printf("Enter rupees:");
+    scanf("%d",&rupees);
+    dollors=rupees/48;
+    printf("dollors=%d",dollors);
+    getch();
+}
