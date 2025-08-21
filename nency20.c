689bd8bf419f8fe9cc8e3b3c9b31c398d2a9f384
raiyaninency2-coder <raiyaninency2@gmail.com>
2025-08-21 19:37:08 +0530

    Add files via upload

---
+++ nency20.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float radius,area;
+    printf("Enter radius:");
+    scanf("%f",&radius);
+    area=(3.14)*radius*radius;
+    printf("area=%f",area);
+    getch();
+}
