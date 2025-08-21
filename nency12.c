f46f79275e5674ebd0fda092e57617b57980fae9
raiyaninency2-coder <raiyaninency2@gmail.com>
2025-08-21 19:31:33 +0530

    Add files via upload

---
+++ nency12.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float grams,kg;
+    printf("Enter grams :");
+    scanf("%f",&grams);
+    kg=grams/1000;
+    printf("kg=%f",kg);
+    getch();
+}
