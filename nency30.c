63f57c2ec3c91054f84f5f27eff4d5271f1c6ca1
raiyaninency2-coder <raiyaninency2@gmail.com>
2025-08-22 15:19:30 +0530

    Add files via upload

---
+++ nency30.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a;
+    printf("enter the value of a");
+    scanf("%d",&a);
+    if(a % 7 == 0)
+    {
+        printf("a is divisible by 7 :");
+
+    }
+    else
+    {
+        printf("a is not divisible by 7:");
+    }
+}
