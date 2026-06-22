6771aeeef20ee6b4a9f030b626247dfeb6c16ced
raiyaninency2-coder <raiyaninency2@gmail.com>
2026-06-22 16:26:37 +0530

    Delete python/python directory

--- python/python/print element of list in single line - Copy - Copy.py
+++
@@ -1,9 +0,0 @@
-# print element of list in single line 
-students = ["nency","niyati","dev"]
-marks = [98,97,92,36.50,37.75,35.50]
-def l(list):
-    for item in list:
-        print(item,end="")
-    return item
-l(students)
-l(marks)
