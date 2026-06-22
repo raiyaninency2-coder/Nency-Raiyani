4ea102e6e774eb82aae4382a89a083b69bc0ad50
raiyaninency2-coder <raiyaninency2@gmail.com>
2026-06-22 16:34:14 +0530

    Delete str pra 1 - Copy - Copy.py

--- str pra 1 - Copy - Copy.py
+++
@@ -1,8 +0,0 @@
-s = input("Enter the string : ")
-count = 0
-for char in s:
-    if char == "a" or char == "e" or char == "i" or char == "o" or char == "u" :
-        count+=1
-    #else:
-       # print("not vowls in this string")
-print(count)
